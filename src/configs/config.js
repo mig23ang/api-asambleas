c159605d974118bcb197889ac8646edfc606ffe7
miguel <miguel.reyesc@usantoto.edu.co>
2022-03-26 18:25:39 -0500

    para subir a amazon con globals ok

--- src/configs/config.js
+++ src/configs/config.js
@@ -3,9 +3,9 @@ config();
 
 export default {
   // MONGODB_URI: "mongodb+srv://eskala:eskala@cluster0.1a38u.mongodb.net/ventus?retryWrites=true&w=majority",
-  //MONGODB_URI: "mongodb+srv://eskala:eskala@cluster0.1a38u.mongodb.net/refugio?retryWrites=true&w=majority",
+  MONGODB_URI: "mongodb+srv://eskala:eskala@cluster0.1a38u.mongodb.net/refugio?retryWrites=true&w=majority",
   // MONGODB_URI: "mongodb://localhost/ventus",
-  MONGODB_URI: "mongodb://localhost/refugio",
+  //MONGODB_URI: "mongodb://localhost/refugio",
   PORT: process.env.PORT || 4000,
   SECRET: 'asambleas-api'
 };
